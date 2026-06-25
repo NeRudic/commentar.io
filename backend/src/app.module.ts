3e058566da6e90f902852d25ddac5c8a017db039
Danil Holikov <break.build.one.minute@gmail.com>
2026-06-25 15:14:23 +0300

    fix(backend): remove broken CommentModule import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DBModule } from './db/db.module';
 import { UserModule } from './user/user.module';
-import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [DBModule, UserModule, CommentModule],
+  imports: [DBModule, UserModule],
 })
 export class AppModule {}
