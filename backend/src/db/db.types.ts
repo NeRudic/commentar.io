e815f2c22434e48c1a88bbf94b58316f44f45cf6
Danil Holikov <break.build.one.minute@gmail.com>
2026-06-25 15:30:09 +0300

    refactor(backend): remove unused CommentRow type

--- backend/src/db/db.types.ts
+++ backend/src/db/db.types.ts
@@ -2,12 +2,3 @@ export interface runResponse {
   lastID: number;
   changes: number;
 }
-
-export interface CommentRow {
-  id: number;
-  text: string;
-  parent_comment_id: number | null;
-  user_email: string;
-  file_path: string | null;
-  created_at: string;
-}
