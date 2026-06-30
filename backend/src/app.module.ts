48942289f43345860612ff322e1061eed2c12525
Danil Holikov <break.build.one.minute@gmail.com>
2026-06-30 13:37:32 +0300

    fix(backend): restore CommentModule and CaptchaModule in AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DBModule } from './db/db.module';
 import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { CaptchaModule } from './captcha/captcha.module';
 
 @Module({
-  imports: [DBModule, UserModule],
+  imports: [DBModule, UserModule, CommentModule, CaptchaModule],
 })
 export class AppModule {}
