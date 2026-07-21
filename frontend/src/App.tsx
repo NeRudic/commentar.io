ea0bce2af526bf77ac2ee402edfa1e1e99e9fbee
Danil Holikov <break.build.one.minute@gmail.com>
2026-07-21 14:49:04 +0300

    fix: move global styles and CSS variables from App.module.css to global.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { ToastProvider } from './context/ToastContext';
 import Blog from './components/Blog/Blog';
 import OnlineCount from './components/OnlineCount/OnlineCount';
-import './App.module.css';
 
 export default function App() {
   return (
